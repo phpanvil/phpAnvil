b4a673bdb04957bc4cff3aa327d88655fde5c8d6
Nick Slevkoff <nick@slevkoff.com>
2013-08-19 09:06:37 -0700

    Added AbstractPlugin to Object Component

---
+++ src/phpAnvil/Component/Object/AbstractPlugin.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the phpAnvil framework.
+ *
+ * (c) Nick Slevkoff <nick@phpanvil.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace phpAnvil\Component\Object;
+
+use phpAnvil\Component\Object\AbstractComponent;
+
+/**
+ * Abstract component that plugs into a pluginable component.
+ */
+abstract class AbstractPlugin extends AbstractComponent
+{
+
+}
