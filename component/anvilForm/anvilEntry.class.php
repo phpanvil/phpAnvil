59e8149c6d3fa930658c68e99874d8bfff238c38
Nick Slevkoff <nick@slevkoff.com>
2013-10-30 15:04:54 -0700

    Added anvilValidationFormControl Abstract Class

--- component/anvilForm/anvilEntry.class.php
+++ component/anvilForm/anvilEntry.class.php
@@ -14,9 +14,8 @@
 require_once PHPANVIL2_COMPONENT_PATH . 'anvilContainer.class.php';
 
 require_once 'anvilEntryType.interface.php';
-require_once 'anvilValidationType.interface.php';
 
-require_once 'anvilFormControl.abstract.php';
+require_once 'anvilValidationFormControl.abstract.php';
 
 
 /**
@@ -28,7 +27,7 @@ require_once 'anvilFormControl.abstract.php';
  * @copyright       Copyright (c) 2010 Nick Slevkoff (http://www.slevkoff.com)
  * @ingroup         phpAnvilTools
  */
-class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInterface, anvilValidationTypeInterface
+class anvilEntry extends anvilValidationFormControlAbstract implements anvilEntryTypeInterface
 {
 
     const SIZE_DEFAULT = 0;
@@ -67,8 +66,6 @@ class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInter
 
     public $type = self::ENTRY_TYPE_NORMAL;
 
-    public $validation = false;
-
     public $value;
 
     public $wrapClass = 'inputWrap';
@@ -90,29 +87,6 @@ class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInter
         'number'
     );
 
-    private $validationFailMessages = array();
-
-    private $validationTypeNames = array(
-        '',
-        'email',
-        'number',
-        'required',
-        'max',
-        'min',
-        'maxlength',
-        'minlength',
-        'pattern',
-        'match',
-        'maxchecked',
-        'minchecked',
-        'regex',
-        'callback',
-        'ajax'
-    );
-
-    private $validationTypes = array();
-
-    private $validationValues = array();
 
 
     public function __construct($id = '', $name = 'unknown', $size = self::SIZE_DEFAULT, $value = '', $properties = null)
@@ -129,19 +103,6 @@ class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInter
     }
 
 
-    public function addValidation($type, $name, $value, $failMessage = '')
-    {
-        $this->validation = true;
-        $this->validationTypes[$name] = $type;
-        $this->validationValues[$name] = $value;
-        $this->validationFailMessages[$name] = $failMessage;
-
-        if ($type == self::VALIDATION_TYPE_EMAIL) {
-            $this->type = self::ENTRY_TYPE_EMAIL;
-        } elseif ($type == self::VALIDATION_TYPE_NUMBER) {
-            $this->type = self::ENTRY_TYPE_NUMBER;
-        }
-    }
 
 
     public function renderContent()
@@ -247,46 +208,7 @@ class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInter
         }
 
         //---- Render Validation -----------------------------------------------
-        if ($this->validation) {
-            foreach ($this->validationTypes as $name => $type) {
-                switch ($type) {
-                    case self::VALIDATION_TYPE_EMAIL:
-                    case self::VALIDATION_TYPE_NUMBER:
-                        break;
-
-                    case self::VALIDATION_TYPE_REQUIRED:
-                        $return .= ' required';
-                        break;
-
-                    case self::VALIDATION_TYPE_MAX:
-                    case self::VALIDATION_TYPE_MIN:
-                    case self::VALIDATION_TYPE_MAX_LENGTH:
-                    case self::VALIDATION_TYPE_MIN_LENGTH:
-                    case self::VALIDATION_TYPE_PATTERN:
-
-                        $return .= ' ' . $this->validationTypeNames[$type];
-                        $return .= '="' . $this->validationValues[$name] . '"';
-                        break;
-
-                    case self::VALIDATION_TYPE_MATCH:
-                    case self::VALIDATION_TYPE_MAX_CHECKED:
-                    case self::VALIDATION_TYPE_MIN_CHECKED:
-                    case self::VALIDATION_TYPE_REGEX:
-                    case self::VALIDATION_TYPE_CALLBACK:
-                    case self::VALIDATION_TYPE_AJAX:
-
-                        $return .= ' data-validation-' . $name . '-' . $this->validationTypeNames[$type];
-                        $return .= '="' . $this->validationValues[$name] . '"';
-                        break;
-                }
-
-                if (!empty($this->validationFailMessages[$name])) {
-                    $return .= ' data-validation-' . $name . '-message';
-                    $return .= '="' . $this->validationFailMessages[$name] . '"';
-                    break;
-                }
-            }
-        }
+        $return .= $this->renderValidationParameters();
 
         $return .= ' />';
 
@@ -327,4 +249,17 @@ class anvilEntry extends anvilFormControlAbstract implements anvilEntryTypeInter
 
         return $return;
     }
+
+
+    public function addValidation($type, $name, $value, $failMessage = '')
+    {
+        parent::addValidation($type, $name, $value, $failMessage);
+
+        if ($type == self::VALIDATION_TYPE_EMAIL) {
+            $this->type = self::ENTRY_TYPE_EMAIL;
+        } elseif ($type == self::VALIDATION_TYPE_NUMBER) {
+            $this->type = self::ENTRY_TYPE_NUMBER;
+        }
+    }
+
 }
--- component/anvilForm/anvilValidationFormControl.abstract.php
+++ component/anvilForm/anvilValidationFormControl.abstract.php
@@ -1,85 +1,94 @@
 <?php
-/**
-* @file
-* @author		Nick Slevkoff <nick@slevkoff.com>
-* @copyright 	Copyright (c) 2010 Nick Slevkoff (http://www.slevkoff.com)
-* @license
-* 	This source file is subject to the new BSD license that is
-* 	bundled with this package in the file LICENSE.txt. It is also
-* 	available on the Internet at:  http://www.phpanvil.com/LICENSE.txt
-* @ingroup 		phpAnvilTools
-*/
+
+require_once 'anvilValidationType.interface.php';
+
+require_once 'anvilFormControl.abstract.php';
 
 
-require_once PHPANVIL2_COMPONENT_PATH .'anvilControl.abstract.php';
+abstract class anvilValidationFormControlAbstract extends anvilFormControlAbstract implements anvilValidationTypeInterface
+{
+
+    public $validation = false;
+
+    private $validationFailMessages = array();
+
+    private $validationTypeNames = array(
+        '',
+        'email',
+        'number',
+        'required',
+        'max',
+        'min',
+        'maxlength',
+        'minlength',
+        'pattern',
+        'match',
+        'maxchecked',
+        'minchecked',
+        'regex',
+        'callback',
+        'ajax'
+    );
+
+    private $validationTypes = array();
+
+    private $validationValues = array();
 
 
-/**
-* Base Form Control Abstract Class
-*
-* @version		1.0
-* @date			8/26/2010
-* @author		Nick Slevkoff <nick@slevkoff.com>
-* @copyright 	Copyright (c) 2010 Nick Slevkoff (http://www.slevkoff.com)
-* @ingroup 		phpAnvilTools
-*/
-abstract class anvilFormControlAbstract extends anvilControlAbstract {
+    public function addValidation($type, $name, $value, $failMessage = '')
+    {
+        $this->validation = true;
+        $this->validationTypes[$name] = $type;
+        $this->validationValues[$name] = $value;
+        $this->validationFailMessages[$name] = $failMessage;
+    }
 
-	const VERSION        = '1.0';
-
-
-	public $defaultButtonID;
-	public $name;
-    public $label;
-
-
-	public function __construct($id = '', $name = '', $properties = null) {
-//		$this->_traceEnabled = $traceEnabled;
-
-//		unset($this->defaultButtonID);
-//		unset($this->name);
-//        unset($this->label);
-
-
-//		$this->addProperty('defaultButtonID', '');
-//		$this->addProperty('name', '');
-//        $this->addProperty('label', '');
-
-		$this->name = $name;
-
-		parent::__construct($id, $properties);
-	}
-
-    public function renderLabel()
+    protected function renderValidationParameters()
     {
         $return = '';
 
-        if ($this->label <> '')
-        {
-            $return .= '<label';
-            if ($this->id <> '')
-            {
-                $return .= ' for="' . $this->id . '"';
+        if ($this->validation) {
+            foreach ($this->validationTypes as $name => $type) {
+                switch ($type) {
+                    case self::VALIDATION_TYPE_EMAIL:
+                    case self::VALIDATION_TYPE_NUMBER:
+                        break;
+
+                    case self::VALIDATION_TYPE_REQUIRED:
+                        $return .= ' required';
+                        break;
+
+                    case self::VALIDATION_TYPE_MAX:
+                    case self::VALIDATION_TYPE_MIN:
+                    case self::VALIDATION_TYPE_MAX_LENGTH:
+                    case self::VALIDATION_TYPE_MIN_LENGTH:
+                    case self::VALIDATION_TYPE_PATTERN:
+
+                        $return .= ' ' . $this->validationTypeNames[$type];
+                        $return .= '="' . $this->validationValues[$name] . '"';
+                        break;
+
+                    case self::VALIDATION_TYPE_MATCH:
+                    case self::VALIDATION_TYPE_MAX_CHECKED:
+                    case self::VALIDATION_TYPE_MIN_CHECKED:
+                    case self::VALIDATION_TYPE_REGEX:
+                    case self::VALIDATION_TYPE_CALLBACK:
+                    case self::VALIDATION_TYPE_AJAX:
+
+                        $return .= ' data-validation-' . $name . '-' . $this->validationTypeNames[$type];
+                        $return .= '="' . $this->validationValues[$name] . '"';
+                        break;
+                }
+
+                if (!empty($this->validationFailMessages[$name])) {
+                    $return .= ' data-validation-' . $name . '-message';
+                    $return .= '="' . $this->validationFailMessages[$name] . '"';
+                    break;
+                }
             }
-            $return .= '>' . $this->label . "</label>\n";
         }
 
         return $return;
     }
 
-
-	public function renderPreClientScript() {
-		$return = '';
-		$return .= parent::renderPreClientScript();
-		return $return;
-	}
-
-	public function renderPostClientScript() {
-		$return = '';
-		$return .= parent::renderPostClientScript();
-		return $return;
-	}
-
 }
-
-?>
